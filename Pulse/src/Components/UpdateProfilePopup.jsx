ee06081c5a266321536c4f180bec043d3898c7d3
Nosa Bill Omo-Enabu <144929198+Greenwich23@users.noreply.github.com>
2025-12-23 01:55:40 +0100

    changed the css casing

--- Pulse/src/Components/UpdateProfilePopup.jsx
+++ Pulse/src/Components/UpdateProfilePopup.jsx
@@ -1,5 +1,5 @@
 import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
-import "./updateprofilePopUp.css";
+import "./UpdateprofilePopUp.css";
 import { FaX } from "react-icons/fa6";
 import StyledButton from "./Buttons";
 import { useState } from "react";
